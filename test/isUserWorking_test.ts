c7817893e5ee15508b5a35236e0626a098a0354e
PhilippePerret <philippe.perret@yahoo.fr>
2025-11-05 04:45:08 +0100

    suppression du faux test

--- test/isUserWorking_test.ts
+++ test/isUserWorking_test.ts
@@ -10,26 +10,30 @@
  *  * Au bout du dixième check, le script s'arrête de lui-même
  * 
  */
-import path from 'path';
-import { isUserWorkingOnProject } from '../lib/server/Activity_isUserWorking';
 
 
-const FOLDER_PATH = path.join(__dirname, 'assets', 'folders', 'Work1');
-// const ilyaQuelquesMinutes = new Date().getTime() - 20 * 60 * 1000;
 
-let nombreFois = 10
 
-async function teste() {
-  return new Promise((ok, ko) => {
-    setInterval(() => {
-      const res = isUserWorkingOnProject(FOLDER_PATH, ilyaQuelquesMinutes);
-      console.log("L'user est ", res ? 'actif' : 'inactif');
-      ilyaQuelquesMinutes = new Date().getTime() - 30 * 1000;
-      nombreFois --;
-      if (nombreFois < 0) {ok(null)};
-    }, 15 * 1000);
-  });
-}
-let ilyaQuelquesMinutes = new Date().getTime() - 30 * 1000;
+// import path from 'path';
+// import { isUserWorkingOnProject } from '../lib/server/Activity_isUserWorking';
 
-await teste();
+
+// const FOLDER_PATH = path.join(__dirname, 'assets', 'folders', 'Work1');
+// // const ilyaQuelquesMinutes = new Date().getTime() - 20 * 60 * 1000;
+
+// let nombreFois = 10
+
+// async function teste() {
+//   return new Promise((ok, ko) => {
+//     setInterval(() => {
+//       const res = isUserWorkingOnProject(FOLDER_PATH, ilyaQuelquesMinutes);
+//       console.log("L'user est ", res ? 'actif' : 'inactif');
+//       ilyaQuelquesMinutes = new Date().getTime() - 30 * 1000;
+//       nombreFois --;
+//       if (nombreFois < 0) {ok(null)};
+//     }, 15 * 1000);
+//   });
+// }
+// let ilyaQuelquesMinutes = new Date().getTime() - 30 * 1000;
+
+// await teste();
